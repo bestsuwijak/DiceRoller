28913f1daf30cf66f8af0cf4ae769ee2101d4003
bestsuwijak <42570047+bestsuwijak@users.noreply.github.com>
2019-08-16 22:02:19 +0700

    Task: Find views efficiently

--- app/src/main/java/buu/informatics/s59160141/diceroller/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160141/diceroller/MainActivity.kt
@@ -9,10 +9,13 @@ import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
@@ -25,7 +28,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomeInt = Random().nextInt(6) + 1
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         val drawableResource = when(randomeInt){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -38,8 +40,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun reset() {
-//        val resultText: TextView  = findViewById(R.id.result_text)
-//        resultText.text = 0.toString()
-//        Toast.makeText(this,"Button clicked", Toast.LENGTH_SHORT).show()
+        diceImage.setImageResource(R.drawable.dice_1)
     }
 }
